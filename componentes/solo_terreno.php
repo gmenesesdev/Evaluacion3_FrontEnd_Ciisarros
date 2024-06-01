a76fd58877fa2bac0a98b0f44ddd150374504f82
Gonzalo Meneses <gonzalo.menesesg@gmail.com>
2024-06-01 01:20:31 -0400

    Se modifica componente solo_terreno.php agregando botón para usar con formulario

--- componentes/solo_terreno.php
+++ componentes/solo_terreno.php
@@ -1,17 +1,22 @@
-<h1 id="parcelas">Parcelas</h1>
-<hr class="featurette-divider" />
-<div class="row featurette">
-    <div class="col-md-7">
-        <h2 id="solo-terreno" class="featurette-heading fw-normal lh-1 text-center pt-3">
-            <!-- <h2 class="featurette-heading fw-normal lh-1"> -->
-            Solo Terreno
-        </h2>
-        <p class="lead pt-5 text-center">
-            Especializados en la venta de parcelas de 5000
-            metros cuadrados.
-        </p>
-    </div>
-    <div class="col-md-5">
-        <img src="img/peter-muscutt-pkg77ZtBCmg-unsplash.jpg" alt="" class="img-fluid rounded" />
+<div class="container pt-5">
+    <h1 id="parcelas">Parcelas</h1>
+    <hr class="featurette-divider" />
+    <div class="row featurette">
+        <div class="col-md-7">
+            <h2 id="solo-terreno" class="featurette-heading fw-normal lh-1 text-center pt-3">
+                <!-- <h2 class="featurette-heading fw-normal lh-1"> -->
+                Solo Terreno
+            </h2>
+            <p class="lead pt-5 text-center">
+                Especializados en la venta de parcelas de 5000
+                metros cuadrados.
+            </p>
+            <div class="text-center">
+                <button class="btn btn-primary" onclick="selectOption('Solo Terreno')">Seleccionar Solo Terrenos</button>
+            </div>
+        </div>
+        <div class="col-md-5">
+            <img src="img/peter-muscutt-pkg77ZtBCmg-unsplash.jpg" alt="" class="img-fluid rounded" />
+        </div>
     </div>
 </div>
